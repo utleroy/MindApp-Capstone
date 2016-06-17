2823a2b4b18a3e38e0732403f231c636cb2e6476
Bryan Hankins <bryanhankins@me.com>
2016-06-16 20:00:33 -0500

    drag div testing

--- app/factories/MindFactory.js
+++ app/factories/MindFactory.js
@@ -22,3 +22,17 @@ var mindItem = function(newItem){
 var addFileToRoot = function(addedObject) {
 	console.log(addedObject);
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
